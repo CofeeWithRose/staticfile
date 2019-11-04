554a4aa5bc5a727e8a3a5d9a8b8d3c0ebbf7bd5c
PH <1836071395@qq.com>
2019-11-04 22:46:35 +0800

    feat:添加启动脚本

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-import app from 'koa'
-console.log('start....')
+import Koa from 'koa'
+import KoaStatic from 'koa-static'
+import path from 'path'
+
+const app = new Koa()
+const root = path.resolve('static')
+console.log(__dirname, root)
+app.use(KoaStatic(root))
+app.listen(3000, ()=> {
+    console.log(`server start`)
+})
